61352b81c5729e65fb365819f4d2a6bc384ee3bc
Tien Nguyen Truong <tien.ngtruong@gmail.com>
2019-10-12 11:15:01 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# MongoDB - Mongoose One-to-One relationship example
+
+For more detail, please visit:
+> [Nodejs & MongoDb: Mongoose One-to-One relationship example](https://bezkoder.com/mongoose-one-to-one-relationship-example/)
+
+## Project setup
+```
+npm install
+```
+
+### Run
+```
+node src/server.js
+```
---
+++ src/models/Identifier.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const CustomerSchema = require("./Customer").CustomerSchema;
+
+const Identifier = mongoose.model(
+  "Identifier",
+  new mongoose.Schema({
+    cardCode: String,
+    // customer: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: "Customer"
+    // }
+    customer: CustomerSchema
+  })
+);
+
+module.exports = Identifier;
